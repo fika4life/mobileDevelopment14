58b1c232bb2d155c69f033227560d7351cecf6a8
fika4life <fistewie@gmail.com>
2014-04-25 15:12:12 +0200

    Dynamic restaurant texts in html doc

--- Lab5/js/myscript.js
+++ Lab5/js/myscript.js
@@ -15,5 +15,74 @@
 		
 		$('#back-btn').on("click", function(){
 			$.mobile.changePage("#page-1", {transition: "slide", reverse: true, changeHash:false});
-		});		
+		});	
+
+
+
+
+
+
+		});
+
+		$(function(){
+			helper = new CBHelper("mopub14_labb5", "ed86229afa03c22b4fd0139c60d0bb5e", new GenericHelper());
+			helper.setPassword(hex_md5("mopub14_labb5"));
+
+		var rest1 = {    "Name": "Mathias Dahlgren Matbaren",
+	    				"ID": "1",
+	    				"Description": "Natural taste and natural produce are essential for natural cuisine",
+	   					 "Address": "Sodra Blasieholmshamnen 6, Stockholm",
+						    "Category": "Swedish",
+						    "Price Range": "$$$$",
+						    "Rating": "5"
+				};
+
+		var rest2 = {
+		    "Name": "Fotografiska",
+		    "ID": "2",
+		    "Description": "Our kitchen pride themselves on the same excellent quality and standard as the rest of the museum.",
+		    "Address": "Stadsgardshamnen 22, Stockholm",
+		    "Category": "Cafe",
+		    "Price Range": "$$",
+		    "Rating": "4"
+			};
+
+		var rest3={
+		    "Name": "Wardshuset Ulla Winbladh",
+		    "ID": "3",
+		    "Description": "This restaurant embodies perfection in an old fashioned manner. Both the exterior & the interior design gives a graceful and appealing impression.",
+		    "Address": "Rosendalsvagen 8, Stockholm",
+		    "Category": "Scandinavian",
+		    "Price Range": "$$$",
+		    "Rating": "3"
+			};	
+
+
+
+		helper.searchDocuments(rest1, "restaurants", function(resp) {
+			if(resp.callStatus && resp.outputData.length == 0){
+					helper.insertDocument("restaurants", rest1, function(resp){
+					console.log(resp.outputString);
+					});
+				}
+			});
+
+		helper.searchDocuments(rest2, "restaurants", function(resp) {
+			if(resp.callStatus && resp.outputData.length == 0){
+					helper.insertDocument("restaurants", rest2, function(resp){
+					console.log(resp.outputString);
+					});
+				}
+			});
+
+		helper.searchDocuments(rest3, "restaurants", function(resp) {
+			if(resp.callStatus && resp.outputData.length == 0){
+					helper.insertDocument("restaurants", rest3, function(resp){
+					console.log(resp.outputString);
+					});
+				}
+			});
+			
+		
+		
 		});
